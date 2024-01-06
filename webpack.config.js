51ec03d54f633470a4709862623ef3288c389102
luisarevalo21 <arevaloluis89@yahoo.com>
2024-01-05 16:29:26 -0800

    configure commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: ["./src/index.js"],
+  // devServer: {
+  //   contentBase: path.join(__dirname, "src"),
+  //   compress: true,
+  //   port: 5500,
+  // },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
